b24becf18b7f7d6f8b8979a3d4575dd0fd4ea5cf
Villy Siu <villysiu@gmail.com>
2025-07-17 17:06:37 -0700

    added days input box

--- frontend/src/components/navbar/SearchBar.js
+++ frontend/src/components/navbar/SearchBar.js
@@ -5,14 +5,33 @@ import {useNavigate} from "react-router-dom";
 import {Form, InputGroup} from 'react-bootstrap';
 import {Search} from "react-bootstrap-icons";
 import LoadingOverlay from "./LoadingOverlay";
-
-
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
+import {Col, Row} from 'react-bootstrap';
+import {PlusCircle, DashCircle, Person} from 'react-bootstrap-icons'
 
 const SearchBar = () => {
     const [destination, setDestination] = useState('');
+    const [adultCount, setAdultCount] = useState(2);
+    const [childCount, setChildCount] = useState(2);
+    const [dayCount, setDayCount] = useState(3);
+    const [dayError, setDayError] = useState('');
+
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
     const navigate = useNavigate();
+
+    const handleDay = (action) => {
+        if(action === '+' && dayCount < 14){
+            setDayCount(d=>d+1);
+        }
+        else if(action === '-' && dayCount > 1){
+            setDayCount(d=>d-1);
+        }
+        else {
+            setDayError('Days must be between 1 to 14.')
+        }
+    }
     const handlePlan = async (e) => {
         e.preventDefault(); //
         if (!destination.trim()) {
@@ -41,15 +60,61 @@ const SearchBar = () => {
     return(
         <div id="search">
             {loading && <LoadingOverlay />}
-            <Form onSubmit={handlePlan}>
-                <InputGroup>
+            
+            <Row className='search-bar-row'>
+                <Col xs={12} sm={4}>    
                     <Form.Control
                         type="text"
-                        placeholder="Enter destination, number of days and people"
+                        placeholder="Destination"
                         value={destination}
                         onChange={(e) => setDestination(e.target.value)}
                     />
-                    <InputGroup.Text>
+                </Col>
+                <Col xs={12} sm={3}>    
+                    <Dropdown>
+                        <Dropdown.Toggle  id="dropdown-basic" className='search-input'>
+                            <span className='search-input-icon'><Person size={24}/></span>
+                            <span className='search-inpit-text'>
+                                <div style={{'fontSize': '10px'}}>Days</div>
+                                <div>{dayCount} Days</div>
+                            </span>
+                            
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                    
+                            <div className='serach-input-dropdown'>
+                                <span className='btn-wrapper'><DashCircle className='minus-btn' onClick={()=>handleDay('-')}/></span>
+                
+                                <span className='day-input'>{dayCount}</span>
+                                <span className='btn-wrapper'><PlusCircle className='plus-btn' onClick={()=>handleDay('+')} /></span>
+                                
+                                
+                                
+                            </div>
+                  
+                            
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Col>
+                <Col xs={12} sm={3}>    
+                    <Dropdown>
+                        <Dropdown.Toggle  id="dropdown-basic" className='search-input'>
+                            {adultCount} Travellers
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu>
+                    
+                            <div style={{'width': '50px', 'height': '100px'}}>
+                                Adult: - 2 + <br/>
+                                Children(age 0-17) -0+
+                            </div>
+                  
+                            
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Col>
+                    {/* <InputGroup.Text>
 
                         <button
                             className="search-btn"
@@ -60,10 +125,13 @@ const SearchBar = () => {
                         </button>
 
 
-                    </InputGroup.Text>
-                </InputGroup>
+                    </InputGroup.Text> */}
+                {/* </InputGroup> */}
+                <Col xs={12} sm={2}>
+                    <Button variant="primary" onClick={handlePlan}>Look up</Button>
+                </Col>
+            </Row>
 
-            </Form>
             {error && <div>{error}</div>}
         </div>
     )
